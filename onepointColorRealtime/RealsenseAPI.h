e4d59fe2830bf3ebed5e7c15e3861caeefca25eb
nenoNaninu <neno@neno-garden.com>
2018-05-26 22:43:47 +0900

    マシになった

--- onepointColorRealtime/RealsenseAPI.h
+++ onepointColorRealtime/RealsenseAPI.h
@@ -4,21 +4,21 @@
 class RealsenseAPI
 {
 public:
-	RealsenseAPI() {}
-	~RealsenseAPI();
-	bool initialize(int width, int height, int fps);
-	void getColorImage(cv::Mat& inputMat)const;
-	int getWidth()const;
-	int getHeight()const;
-	int getFPS()const;
+    RealsenseAPI() {}
+    ~RealsenseAPI();
+    bool initialize(int width, int height, int fps);
+    void getColorImage(cv::Mat& inputMat)const;
+    int getWidth()const;
+    int getHeight()const;
+    int getFPS()const;
 
 private:
 
-	int width, height, fps;
-	//namespace Intel::RealSense::Base で
-	//private:
-	//	/* Call Release() to delele object */
-	//	void operator delete(void* pthis);
-	//とか書かれているのでスマポは使えない。
-	Intel::RealSense::SenseManager* senseManager = nullptr;
+    int width, height, fps;
+    //namespace Intel::RealSense::Base で
+    //private:
+    //	/* Call Release() to delele object */
+    //	void operator delete(void* pthis);
+    //とか書かれているのでスマポは使えない。
+    Intel::RealSense::SenseManager* senseManager = nullptr;
 };
